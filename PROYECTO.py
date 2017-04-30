026ae0dd39f61439779d9d3a8721fc1d4db8dc1c
tatiana629 <tatiana629@javerianacali.edu.co>
2017-04-30 16:14:28 -0500

    Update PROYECTO.py

--- PROYECTO.py
+++ PROYECTO.py
@@ -5,13 +5,13 @@ def numerocien():
         if numero == 100:
             print("El número es 100")
             intentos = 10000
-        else:
+        else
             if numero > 100:
                 print("El número es menor que", numero)
                 numero = int(input("Intenta de nuevo: "))
             elif type(numero) == str:
                 print("Debe ser un número")
-                numero = int(input("Otro intento: "))
+                numero = int(input("Otro intento: "
             else:
                 print("El número es mayor que", numero)
                 numero = int(input("Sigue intentando: "))
